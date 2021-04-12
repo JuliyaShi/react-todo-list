400835e76643e5004949e957dc141238a2b46827
JuliyaShi <jvshirobokova@gmail.com>
2021-04-12 11:28:05 +0200

    fixed:all done

---
+++ build/precache-manifest.4784f19cbbfa28ad981788cf72c9eafb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d33fa4423816c496b182",
+    "url": "/static/css/main.0dffc585.chunk.css"
+  },
+  {
+    "revision": "d33fa4423816c496b182",
+    "url": "/static/js/main.d33fa442.chunk.js"
+  },
+  {
+    "revision": "67147b652a18dc56ffc3",
+    "url": "/static/js/1.67147b65.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ef995005a79fa5e1d6f92dca4de1fbf",
+    "url": "/index.html"
+  }
+];
